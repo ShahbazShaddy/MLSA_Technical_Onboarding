b8608c71d65da2aa1cf8a999580f0b04b1f948f5
Shahbaz Ghafil <shahbazghafil@gmail.com>
2023-09-12 23:55:39 +0500

    add all commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Shahbaz Ali",
+  title: "Web Designer & .Net Developer",
+  email: "shahbazghafil@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
